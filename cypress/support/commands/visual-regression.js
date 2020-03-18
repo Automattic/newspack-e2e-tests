95df7d6ada2c1f4b8466761924bb156acb48e2c9
Adam Borowski <adam@adamboro.com>
2020-03-18 13:33:00 +0100

    feat(visual-regression): adjust threshold

--- cypress/support/commands/visual-regression.js
+++ cypress/support/commands/visual-regression.js
@@ -2,13 +2,12 @@ const compareSnapshotCommand = require("cypress-visual-regression/dist/command")
 
 compareSnapshotCommand();
 
-// take a visual regression screenshot only
 Cypress.Commands.add(
   "compareVisualRegressionScreenshot",
   (element, screenshotName) => {
     if (Cypress.env("type")) {
       Cypress.$("#wpadminbar").hide();
-      cy.get(element).compareSnapshot(screenshotName);
+      cy.get(element).compareSnapshot(screenshotName, 0.05);
     }
   }
 );
