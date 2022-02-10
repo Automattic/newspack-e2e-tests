a06b37c6bb868996b259649ae4bea4985959fee7
Adam Borowski <adam@adamboro.com>
2022-02-10 10:56:52 +0100

    feat: install alpha if available

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -25,7 +25,15 @@ function install_plugin() {
   if [[ $? == 1 ]]; then
     if [[ "$PLUGIN_NAME" =~ ^newspack-* ]]; then
       log "Installing Newspack plugin: $PLUGIN_NAME"
-      wp plugin install https://github.com/Automattic/$PLUGIN_NAME/releases/latest/download/$PLUGIN_NAME.zip --force --allow-root
+      LATEST_PRERELEASE_TAG=$(curl -s "https://api.github.com/repos/Automattic/$PLUGIN_NAME/releases" | jq -r 'first | if .prerelease == true then .tag_name else "null" end')
+      if [ "$LATEST_PRERELEASE_TAG" == "null" ]; then
+        log "Last release of $PLUGIN_NAME is not a prerelease, installing latest release."
+        wp plugin install https://github.com/Automattic/$PLUGIN_NAME/releases/latest/download/$PLUGIN_NAME.zip --force --allow-root
+      else
+        log "Installing alpha version: $PLUGIN_NAME@$LATEST_PRERELEASE_TAG"
+        ALPHA_PLUGIN_NAME="$PLUGIN_NAME-$LATEST_PRERELEASE_TAG"
+        wp plugin install https://github.com/Automattic/$PLUGIN_NAME/releases/download/$LATEST_PRERELEASE_TAG/$PLUGIN_NAME.zip  --force --allow-root
+      fi
     else
       log "Installing $PLUGIN_NAME"
       wp plugin install $PLUGIN_NAME --force --allow-root
