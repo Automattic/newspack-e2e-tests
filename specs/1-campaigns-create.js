670af1bdbf589e15a5ab94e0ad892e688ed0c836
Adam Borowski <adam@adamboro.com>
2022-02-11 12:04:13 +0100

    lets see if the prompt is there at all

--- specs/1-campaigns-create.js
+++ specs/1-campaigns-create.js
@@ -39,6 +39,6 @@ describe("Campaigns", () => {
     cy.visitWPURL("/");
 
     cy.get(".entry-title a").first().click();
-    cy.get(".newspack-popup").contains(promptContent).should("be.visible");
+    cy.get(".newspack-popup").contains(promptContent);
   });
 });
