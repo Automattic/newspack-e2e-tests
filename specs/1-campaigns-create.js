d6b64c3b93bf312ae98e684cf9e3dd1c70c82114
Adam Borowski <adam@adamboro.com>
2022-02-11 11:43:31 +0100

    increase timeout for campaigns test

--- specs/1-campaigns-create.js
+++ specs/1-campaigns-create.js
@@ -39,6 +39,8 @@ describe("Campaigns", () => {
     cy.visitWPURL("/");
 
     cy.get(".entry-title a").first().click();
-    cy.get(".newspack-popup").contains(promptContent).should('be.visible');
+    cy.get(".newspack-popup")
+      .contains(promptContent, { timeout: 60000 })
+      .should("be.visible");
   });
 });
