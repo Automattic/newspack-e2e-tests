ebe0190f5cad6e357edbbdf663732ae11cb5dea5
Adam Borowski <adam@adamboro.com>
2022-02-17 17:38:59 +0100

    test: suppress some JS errors

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,3 +15,17 @@ import "./commands";
 Cypress.Cookies.defaults({
   preserve: /\.*/,
 });
+
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // Ignore errors matching these fragments.
+  const matchedErrors = [
+    // Errors appearing when opening core editor to edit a prompt.
+    "setting 'mejs'",
+    "mejs is not defined",
+    "mediaelementplayer",
+  ].filter((fragment) => err.message.includes(fragment));
+  if (matchedErrors.length) {
+    // Returning false here prevents Cypress from failing the test.
+    return false;
+  }
+});
