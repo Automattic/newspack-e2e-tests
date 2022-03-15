3534c260efe2902a23a93ca16deafd6a15b68f6e
Adam Boro <adam@adamboro.com>
2022-03-15 13:05:38 +0100

    feat: test donations; add full domain; other improvements (#9)

--- cypress/support/commands/index.js
+++ cypress/support/commands/index.js
@@ -1,10 +1,12 @@
 import "./wordpress";
+import "./newspack";
 import "./visual-regression";
 
-Cypress.Commands.add("selectOption", (label, value, expected) => {
-  cy.contains(label).siblings("select").select(value);
-
-  expected && cy.contains(expected);
+Cypress.Commands.add("selectOption", (label, value) => {
+  cy.contains("label", label)
+    .invoke("attr", "for")
+    .then((id) => cy.get(`#${id}`))
+    .select(value);
 });
 
 Cypress.Commands.add("fillInput", (label, value) => {
@@ -15,6 +17,31 @@ Cypress.Commands.add("fillInput", (label, value) => {
     .should("have.value", value);
 });
 
+Cypress.Commands.add("fillInputByPlaceholder", (placeholder, value) => {
+  cy.get(`input[placeholder="${placeholder}"]`)
+    .clear()
+    .type(value)
+    .should("have.value", value);
+});
+
+Cypress.Commands.add("verifyInputValue", (label, value) => {
+  cy.contains(label).siblings("input").should("have.value", value);
+});
+
+Cypress.Commands.add("checkInput", (label) => {
+  cy.contains("label", label)
+    .invoke("attr", "for")
+    .then((id) => cy.get(`#${id}`))
+    .check();
+});
+
 Cypress.Commands.add("assertURLIncludes", (value) => {
   cy.url().should("include", value);
+});
+
+Cypress.Commands.add("getIframe", (selector) => {
+  cy.get(selector)
+    .its("0.contentDocument.body")
+    .should("not.be.empty")
+    .then(cy.wrap);
 });
---
+++ specs/2-donations-setup.js
@@ -0,0 +1,89 @@
+describe("Donations", () => {
+  it("Visit the Donations wizard", () => {
+    cy.visitNewspackWizard("Reader Revenue");
+  });
+
+  it("Configure Stripe as platform", () => {
+    // Warning is shown initially.
+    cy.contains("The required plugins are not installed and activated.");
+    // Wait for a WC install prompt.
+    cy.contains("WooCommerce");
+
+    cy.selectOption("Select Reader Revenue Platform", "stripe");
+    cy.contains("a", "Stripe Settings").click();
+
+    // Warning is shown initially.
+    cy.contains("Stripe secret key not provided.");
+
+    // Set Stripe credentials.
+    cy.checkInput("Use Stripe in test mode");
+    cy.fillInput("Publishable Key", Cypress.env("STRIPE_PUBLISHABLE_KEY"));
+    cy.fillInput("Secret Key", Cypress.env("STRIPE_SECRET_KEY"));
+
+    cy.contains("Save Settings").click();
+  });
+
+  it("Configure donations settings", () => {
+    cy.contains("a", "Donations").click();
+    // Warning is shown initially.
+    cy.contains(
+      "Your donations landing page has been created, but is not yet published."
+    );
+    cy.verifyInputValue("Suggested donation amount per month", "15");
+    cy.checkInput("Set exact monthly donation tiers");
+    cy.verifyInputValue("Low-tier", "7");
+    cy.fillInput("Low-tier", "2");
+    cy.verifyInputValue("Mid-tier", "15");
+    cy.fillInput("Mid-tier", "21");
+    cy.verifyInputValue("High-tier", "30");
+    cy.fillInput("High-tier", "42");
+
+    cy.contains("Save Settings").click();
+    cy.verifyInputValue("Low-tier", "2");
+    cy.verifyInputValue("Mid-tier", "21");
+    cy.verifyInputValue("High-tier", "42");
+  });
+
+  it("Configure donations page", () => {
+    cy.contains("a", "Edit Page").click();
+    cy.wpDismissBlockEditorIntro();
+    cy.wpPublishPost("Page");
+    cy.contains("Log Out").click({ force: true });
+  });
+
+  it("Make a donation as a non-logged-in user", () => {
+    cy.visitWPURL("/support-our-publication/");
+
+    cy.contains("Support our publication");
+    cy.contains("button", "Donate with card").click();
+
+    const stripeIFrameSelector =
+      "iframe[title='Secure card payment input frame']";
+    cy.getIframe(stripeIFrameSelector)
+      .find('input[placeholder="Card number"]')
+      .type("4242424242424242");
+    cy.getIframe(stripeIFrameSelector)
+      .find('input[placeholder="MM / YY"]')
+      .type("1232");
+    cy.getIframe(stripeIFrameSelector)
+      .find('input[placeholder="CVC"]')
+      .type("222");
+    cy.getIframe(stripeIFrameSelector)
+      .find('input[placeholder="ZIP"]')
+      .type("12345");
+
+    const uniqId = Math.round(Math.random() * 999999);
+    const donorEmail = `tester-${uniqId}@testers.com`;
+    cy.fillInputByPlaceholder("Email", donorEmail);
+    cy.fillInputByPlaceholder("Full Name", "Tester Testerson");
+
+    cy.contains("button", "Donate with card").click();
+
+    cy.contains("Processing payment…");
+
+    cy.contains(
+      `Your payment has been processed. Thank you for your contribution! You will receive a confirmation email at ${donorEmail}.`,
+      { timeout: 120000 }
+    );
+  });
+});
